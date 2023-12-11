b21c51ac9d80fee9cde6d28f2d8ca3deee4504b9
geronimo3tr <jocelyn.lepagepro@gmail.com>
2023-12-11 17:43:33 +0100

    purgecss + optimize bootstrap code

---
+++ purgecss.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  content: ["index.html", "assets/bootstrap/bootstrap.bundle.js"],
+  css: ["assets/bootstrap/bootstrap.css"],
+  safelist: [".carousem-item-start"],
+  output: "assets/optimize_bootstrap/optimize_bootstrap.css",
+};
