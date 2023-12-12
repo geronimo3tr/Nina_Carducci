8a9e99002348f6e7a37139be0ad26b657af263f4
geronimo3tr <jocelyn.lepagepro@gmail.com>
2023-12-12 04:22:13 +0100

    fix gallery

--- purgecss.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  content: ["index.html", "assets/bootstrap/bootstrap.bundle.js"],
-  css: ["assets/bootstrap/bootstrap.css"],
-  safelist: [".carousem-item-start"],
-  output: "assets/bootstrap/bootstrap.min.css",
-};
