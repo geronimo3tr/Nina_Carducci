8693d2a5bcaa2e726a83b1e3603fc1fa3b1fd91b
geronimo3tr <jocelyn.lepagepro@gmail.com>
2023-12-12 01:46:59 +0100

    bootstrap.min

--- purgecss.config.js
+++ purgecss.config.js
@@ -2,5 +2,5 @@ module.exports = {
   content: ["index.html", "assets/bootstrap/bootstrap.bundle.js"],
   css: ["assets/bootstrap/bootstrap.css"],
   safelist: [".carousem-item-start"],
-  output: "assets/optimize_bootstrap/optimize_bootstrap.css",
+  output: "assets/bootstrap/bootstrap.min.css",
 };
